2c3dde180eaf9131836e43a842349fc01085976b
James Goatcher <jamesgoatcher@gmail.com>
2016-07-12 14:48:29 -0700

    NG-Views are working.  Home attached to index.  Signup View working.  Created partials for login and search too.  Updated styling.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
 var app = angular.module('MagicDeckApp', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode({enabled:true});
+
+	$routeProvider.when('/', {
+		templateUrl: 'partials/home.html'
+	}).when('/signup', {
+		templateUrl: 'partials/signup.html'
+	}).when('/search', {
+		templateUrl: 'partials/search.html'
+	}).when('/login', {
+		templateUrl: 'partials/login.html'
+	});
+}]);
