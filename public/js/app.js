e07f73e17fd07ac6b014dcb71ba007ec8e4940cb
Samuel Weisse <samuelweisse@Generals-MacBook-Air-5.local>
2016-07-15 07:42:34 -0700

    fix to rerouting and change to where routed

--- public/js/app.js
+++ public/js/app.js
@@ -86,7 +86,7 @@ app.controller('SignUp', ['$http', '$scope', '$location', '$window', function($h
 				$scope.$emit('getUser', {
 					userLogged: userLogged
 			});
-				$location.url('/');
+				$location.url('/users/'+userLogged._id);
 			} else {
 				console.log('Username already exists pls try again')
 			}
@@ -116,7 +116,7 @@ app.controller('LogIn', ['$http', '$scope', '$location', '$route', function($htt
 				userLogged: userLogged
 			});
 			console.log('redirecting to home');
-			$location.url('/');
+			$location.url('/users/'+userLogged._id);
 		}).then(function(err){
 			$scope.badPassword = true;
 		});
